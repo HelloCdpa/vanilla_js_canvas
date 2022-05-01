c18698fb6dd29afccf80c97c5e406fce88b78e8d
HelloCdpa <92144783+HelloCdpa@users.noreply.github.com>
2022-05-01 17:58:49 +0900

    js

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
+ctx.strokeStyle = "##2c2c2c";
+ctx.lineWidth = 2.5;
 
 
 let painting = false;
 
 function stopPainting(){
     painting = false;
+}
+
+function startPainting(){
+    painting = true;
 }
 
 function onMouseMove(event){
@@ -25,7 +31,7 @@ function onMouseUp(event){
 
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
-    canvas.addEventListener("mousedown",onMouseDown);
-    canvas.addEventListener("mouseup",onMouseUp);
+    canvas.addEventListener("mousedown",startPainting);
+    canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
 }
