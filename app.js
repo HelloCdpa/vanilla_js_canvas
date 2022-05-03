8958b4f8a9208f39d0a0e72e1383c359e319912d
HelloCdpa <92144783+HelloCdpa@users.noreply.github.com>
2022-05-03 14:51:30 +0900

    filling

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode")
 
-ctx.strokeStyle = "##2c2c2c";
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
+
 let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting = false;
@@ -38,6 +46,30 @@ function onMouseUp(event){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+    }
+    
+}
+
+// 채우기를 눌렀을 때 버튼의 변화
+function handleModeClick(){
+    if(filling===true){
+        filling = false;
+        mode.innerText = "Fill";
+    }else{
+        filling = true;
+        mode.innerText = "Paint";
+    }
 }
 
 if(canvas){
@@ -45,8 +77,18 @@ if(canvas){
     canvas.addEventListener("mousedown",startPainting);
     canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
+    canvas.addEventListener("click",handleCanvasClick);
 }
 
 
 // 컬러선택 Array를 대표하는 이름 : anyname
 Array.from(colors).forEach(anyname =>anyname.addEventListener("click", handleColorClick));
+
+// 브러쉬 사이즈 조절
+if(range){
+    range.addEventListener("input", handleRangeChange)
+}
+
+if(mode){
+    mode.addEventListener("click",handleModeClick)
+}
